59341181692fd0455659e4bf4c667b84243c6310
Sehmer Desktop <matheustae@hotmail.com>
2022-12-31 10:07:11 -0300

    Finish

--- niednotas/settings.py
+++ niednotas/settings.py
@@ -81,17 +81,23 @@ WSGI_APPLICATION = 'niednotas.wsgi.application'
 import psycopg2
 import dj_database_url
 
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nied_notas',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'nied_notas',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost'
+#     }
+# }
 # <<<<<<< HEAD
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
